e5578d58fa4f8d75c3d0f0df8f03f4368ff599a4
Marcelo <mrcelo014@gmail.com>
2024-06-02 18:25:34 -0300

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main() {
+    int x;
+    x = 5;
+
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << x;
+
+    return 0;
+}
