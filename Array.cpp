ade9f90e014f2a9945fff9eae7b01cae2f4872e2
Marcelo <mrcelo014@gmail.com>
2024-06-04 15:43:46 -0300

    sla

--- Array.cpp
+++ Array.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 int main() {
-    std::string cars[] = {"Corvett", "Mustang", "Camry"};
+    std::string cars[] = { "Corvett", "Mustang", "Camry" };
     int numbers[3];
 
     cars[2] = "Camaro";
 
     std::cout << cars[0] << std::endl;
 
-    std::string students[] = {"student1", "student2", "student3"};
+    std::string students[] = { "student1", "student2", "student3" };
 
     for (int i = 0; i < sizeof(students)/sizeof(std::string); i++) {
         std::cout << students[i] << std::endl;
